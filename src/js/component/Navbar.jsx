7531619ac0e44791cf11b316ed4821b9cddaa92e
Oscar Urrutia <oscarurrutia940@gmail.com>
2022-08-10 09:23:06 +0000

    hamburguer collapse :)

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 
 const Navbar = (props) => {
@@ -7,14 +6,14 @@ const Navbar = (props) => {
 
 
   return (<nav class=" container-fluid navbar navbar-expand-lg navbar-dark bg-dark">
-    <div className="  container ">
+    <div className="  container col-12 d-flex justify-content-around ">
       <div className="container col-10"><a className="navbar-brand" href="#">Start Bootstrap</a></div>
 
       <div className="container col-2">
         <div className="container"><button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button></div>
-        <div className=" d-flex  collapse navbar-collapse" id="navbarNavDropdown">
+        <div className="navbar-collapse collapse justify-content-end" id="navbarNavDropdown">
           <ul className="navbar-nav">
             <li className="nav-item">
               <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
